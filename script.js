9cbbe373374252c278ce65fc9f1dcfab22030acc
abhinavanand-fed <abhinavanand080@gmail.com>
2022-12-17 20:14:54 +0530

    Deleted script.js and made changes in index.html

--- script.js
+++
@@ -1,9 +0,0 @@
-function countCharacters(){
-    const inputTextLength = document.getElementById("inputText").value.length
-    document.getElementById("displayCharaterCount").innerHTML = "The count of character is " + inputTextLength;
-}
-
-function countCharacterOnClickOfButton(){
-    const inputTextLength = document.getElementById("inputText2").value.length
-    document.getElementById("displayCharaterCount").innerHTML = "The count of character is " + inputTextLength;
-}
